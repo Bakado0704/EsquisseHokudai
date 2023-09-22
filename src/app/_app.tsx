b2d1ea75348465c4931ccde866418bdb11ef328e
unknown <kado_hiroki@yahoo.co.jp>
2023-09-22 19:00:47 +0900

    修正

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -5,8 +5,6 @@ import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  console.log("test");
-
   return (
     <Provider store={store}>
       <Component {...pageProps} />
--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -7,7 +7,7 @@ import {
 } from "@/categoryData/categoryData";
 import NavFooter from "@/components/nav/NavFooter/NavFooter";
 import NavHeader from "@/components/nav/NavHeader/NavHeader";
-import { getImage, postSubmit } from "@/helpers/api-util";
+import { postSubmit } from "@/helpers/api-util";
 import storage from "@/helpers/firebase";
 import {
   getDownloadURL,
@@ -18,23 +18,20 @@ import {
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { FormEvent, useRef, useState } from "react";
-import styled from "styled-components";
+import { FormEvent, useState } from "react";
+import styled, { css } from "styled-components";
 
 export default function Page() {
-  const titleInputRef = useRef<HTMLInputElement | null>(null);
-  const descriptionInputRef = useRef<HTMLInputElement | null>(null);
-
   const router = useRouter();
   const [loading, setLoading] = useState(false);
   const [isUploaded, setIsUploaded] = useState(false);
   const [imageSource, setImageSource] = useState<string | StaticImport>();
   const [imageName, setImageName] = useState<string>();
+  const [title, setTitle] = useState<string>();
+  const [description, setDescription] = useState<string>();
 
   async function submitFormHandler(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    const enteredTitle = titleInputRef.current?.value;
-    const enteredDescription = descriptionInputRef.current?.value;
 
     let projectCategoryChecked = [];
     let buildingCategoryChecked = [];
@@ -69,10 +66,10 @@ export default function Page() {
 
     await postSubmit(
       //@ts-ignore
-      enteredTitle,
+      title,
       { projectCategoryChecked, buildingCategoryChecked, toolCategoryChecked },
       imageName,
-      enteredDescription
+      description
     ).then((data) => router.push("/home"));
   }
 
@@ -110,103 +107,144 @@ export default function Page() {
     );
   };
 
+  const titleHandler = () => {
+    //@ts-ignore
+    const enteredTitle = document.getElementById("title").value;
+
+    setTitle(enteredTitle);
+  };
+
+  const descriptionHandler = () => {
+    //@ts-ignore
+    const enteredDescription = document.getElementById("description").value;
+
+    setDescription(enteredDescription);
+  };
+
   return (
     <>
       <NavHeader />
       <Wrapper>
         <WrapperInner>
-          <h1>投稿</h1>
+          <P $home={true}>投稿</P>
           <form onSubmit={submitFormHandler}>
-            <div>
-              <label htmlFor="title">タイトル</label>
-              <input type="text" id="title" ref={titleInputRef} />
-            </div>
-            <ul>
-              {projectCategory.map((category) => {
-                return (
-                  <li key={category.id[0]}>
-                    <label htmlFor="title">
-                      <input
-                        type="checkbox"
-                        id={category.id[0]}
-                        name={category.id[0]}
-                        value={category.id[0]}
-                      />
-                      {category.title}
-                    </label>
-                  </li>
-                );
-              })}
-            </ul>
-            <ul>
-              {buildingCategory.map((category) => {
-                return (
-                  <li key={category.id[0]}>
-                    <label htmlFor="title">
-                      <input
-                        type="checkbox"
-                        id={category.id[0]}
-                        name={category.id[0]}
-                        value={category.id[0]}
-                      />
-                      {category.title}
-                    </label>
-                  </li>
-                );
-              })}
-            </ul>
-            <ul>
-              {toolCategory.map((category) => {
-                return (
-                  <li key={category.id[0]}>
-                    <label htmlFor="title">
-                      <input
-                        type="checkbox"
-                        id={category.id[0]}
-                        name={category.id[0]}
-                        value={category.id[0]}
-                      />
-                      {category.title}
-                    </label>
-                  </li>
-                );
-              })}
-            </ul>
-            <div>
-              <label htmlFor="image">写真</label>
-              <input
-                className="imageUploadInput"
-                multiple
-                name="imageURL"
-                type="file"
-                accept=".png, .jpeg, .jpg"
-                onChange={onFileUploadToFirebase}
+            <Div $wrapper={true}>
+              <Label $left={true} htmlFor="title">
+                タイトル
+              </Label>
+              <Textarea
+                id="text"
+                value={title}
+                rows={1}
+                onChange={titleHandler}
               />
-            </div>
+            </Div>
+            <Div $wrapper={true}>
+              <P $left={true}>カテゴリ</P>
+              <Div>
+                <Ul>
+                  {projectCategory.map((category) => {
+                    return (
+                      <li key={category.id[0]}>
+                        <label htmlFor="title">
+                          <Input
+                            type="checkbox"
+                            id={category.id[0]}
+                            name={category.id[0]}
+                            value={category.id[0]}
+                          />
+                          {category.title}
+                        </label>
+                      </li>
+                    );
+                  })}
+                </Ul>
+                <Ul>
+                  {buildingCategory.map((category) => {
+                    return (
+                      <li key={category.id[0]}>
+                        <label htmlFor="title">
+                          <Input
+                            type="checkbox"
+                            id={category.id[0]}
+                            name={category.id[0]}
+                            value={category.id[0]}
+                          />
+                          {category.title}
+                        </label>
+                      </li>
+                    );
+                  })}
+                </Ul>
+                <Ul>
+                  {toolCategory.map((category) => {
+                    return (
+                      <li key={category.id[0]}>
+                        <label htmlFor="title">
+                          <Input
+                            type="checkbox"
+                            id={category.id[0]}
+                            name={category.id[0]}
+                            value={category.id[0]}
+                          />
+                          {category.title}
+                        </label>
+                      </li>
+                    );
+                  })}
+                </Ul>
+              </Div>
+            </Div>
 
-            {loading ? (
-              <h2>アップロード中・・・</h2>
-            ) : (
-              <>
-                {isUploaded && (
-                  <>
-                    {imageSource && (
-                      <Image
-                        src={imageSource}
-                        alt="chrch"
-                        width={300}
-                        height={200}
-                      />
+            <Div $wrapper={true}>
+              <Label $left={true} htmlFor="image">
+                写真
+              </Label>
+              <Div $right={true}>
+                <input
+                  className="imageUploadInput"
+                  multiple
+                  name="imageURL"
+                  type="file"
+                  accept=".png, .jpeg, .jpg"
+                  onChange={onFileUploadToFirebase}
+                />
+                <Div $photo={true}>
+                  <Div $photoinner={true}>
+                    {loading ? (
+                      <h2>アップロード中・・・</h2>
+                    ) : (
+                      <>
+                        {isUploaded && (
+                          <>
+                            {imageSource && (
+                              <Image
+                                src={imageSource}
+                                alt="church"
+                                layout={"fill"}
+                                objectFit={"cover"}
+                              />
+                            )}
+                          </>
+                        )}
+                      </>
                     )}
-                  </>
-                )}
-              </>
-            )}
-            <div>
-              <label htmlFor="description">内容</label>
-              <input type="text" id="description" ref={descriptionInputRef} />
-            </div>
-            <button>投稿する</button>
+                  </Div>
+                </Div>
+              </Div>
+            </Div>
+            <Div $wrapper={true}>
+              <Label $left={true} htmlFor="description">
+                内容
+              </Label>
+              <Textarea
+                id="description"
+                value={description}
+                rows={5}
+                onChange={descriptionHandler}
+              />
+            </Div>
+            <Button>投稿する</Button>
           </form>
         </WrapperInner>
       </Wrapper>
@@ -226,9 +264,242 @@ const Wrapper = styled.div`
   align-items: center;
   justify-content: center;
   overflow-y: hidden;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 const WrapperInner = styled.div`
   width: 100%;
   height: 100%;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const Ul = styled.ul<{ $article?: boolean }>`
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+
+  &:first-child {
+    margin-top: 0;
+  }
+`;
+
+const Input = styled.input`
+  --border-color: #9fa0a0;
+  --background-color: #434141;
+
+  cursor: pointer;
+  padding-left: 30px;
+  vertical-align: middle;
+  position: relative;
+
+  &::before,
+  &::after {
+    content: "";
+    display: block;
+    position: absolute;
+  }
+
+  &::before {
+    border-color: var(--border-color);
+    background-color: var(--background-color);
+    border: 3px solid #666464;
+    width: 20px;
+    height: 20px;
+    transform: translate(-50%, -50%);
+    top: 50%;
+    left: 50%;
+    border-radius: 50%;
+  }
+
+  &::after {
+    opacity: 0;
+    width: 10px;
+    height: 10px;
+    background-color: var(--border-color);
+    transform: translate(-50%, -50%);
+    top: 50%;
+    left: 50%;
+    border-radius: 50%;
+  }
+
+  &:checked::after {
+    opacity: 1;
+  }
+`;
+
+const Div = styled.div<{
+  $photo?: boolean;
+  $photoinner?: boolean;
+  $wrapper?: boolean;
+  $right?: boolean;
+}>`
+  --color-background: #c9caca;
+
+  ${(props) =>
+    props.$wrapper &&
+    css`
+      width: 100%;
+      display: flex;
+      align-items: center;
+      padding-top: 16px;
+      padding-bottom: 16px;
+      border-bottom: 1px solid white;
+    `}
+
+  ${(props) =>
+    props.$photo &&
+    css`
+      width: 100%;
+      height: 600px;
+      padding-right: 80px;
+      padding-left: 80px;
+      margin-top: 8px;
+      background-color: var(--color-background);
+    `}
+
+  ${(props) =>
+    props.$photoinner &&
+    css`
+      position: relative;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    `}
+
+    ${(props) =>
+    props.$right &&
+    css`
+      width: 90%;
+    `}
+`;
+
+const Label = styled.label<{
+  $left?: boolean;
+}>`
+  ${(props) =>
+    props.$left &&
+    css`
+      width: 10%;
+      text-align: center;
+    `}
+`;
+
+const P = styled.p<{
+  $title?: boolean;
+  $tag?: boolean;
+  $home?: boolean;
+  $description?: boolean;
+  $esquisse?: boolean;
+  $name?: boolean;
+  $left?: boolean;
+}>`
+  --border-color: #9fa0a0;
+  --background-color: #434141;
+
+  margin-top: 10px;
+  font-size: 16px;
+
+  ${(props) =>
+    props.$description &&
+    css`
+      display: block;
+      padding: 6px;
+      font-size: 16px;
+    `}
+
+  ${(props) =>
+    props.$esquisse &&
+    css`
+      display: block;
+      padding: 16px;
+      font-size: 16px;
+      background-color: var(--background-color);
+      border: solid 3px var(--border-color);
+      border-radius: 8px;
+    `}
+  
+
+  ${(props) =>
+    props.$name &&
+    css`
+      text-align: center;
+    `}
+
+  ${(props) =>
+    props.$home &&
+    css`
+      margin-top: 0;
+      font-size: 32px;
+      text-decoration: underline;
+      text-align: center;
+    `}
+
+  ${(props) =>
+    props.$tag &&
+    css`
+      margin-right: 10px;
+      margin-left: 5px;
+      margin-top: 0;
+    `}
+
+    ${(props) =>
+    props.$title &&
+    css`
+      font-size: 24px;
+      text-decoration: underline;
+      text-align: left;
+    `}
+
+    ${(props) =>
+    props.$left &&
+    css`
+      width: 10%;
+      text-align: center;
+    `}
+`;
+
+const Textarea = styled.textarea`
+  --border-color: #9fa0a0;
+  --background-color: #434141;
+
+  display: block;
+  width: 90%;
+  padding: 6px;
+  font-size: 14px;
+  margin-top: 8px;
+  background-color: var(--background-color);
+  border: solid 3px var(--border-color);
+  border-radius: 8px;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const Button = styled.button`
+  --border-color: #c9caca;
+  --text-color: #323131;
+
+  display: block;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  padding-right: 32px;
+  padding-left: 32px;
+  background-color: white;
+  color: var(--text-color);
+  border: solid 2px var(--border-color);
+  border-radius: 10px;
+  margin: 32px auto 0;
 `;
